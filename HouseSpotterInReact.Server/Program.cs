6f03de91e573845f60199a8d9ba162f69d703744
Patrikas Adomaitis <ado.patrikas@gmail.com>
2024-02-27 22:37:08 +0200

    Minor errors fixed

--- HouseSpotterInReact.Server/Program.cs
+++ HouseSpotterInReact.Server/Program.cs
@@ -1,3 +1,6 @@
+using HouseSpotter.Scrapers;
+using HouseSpotter.Utils;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ScraperClient>();
+builder.Services.AddScoped<ScraperForAruodas>();
 
 var app = builder.Build();
 
