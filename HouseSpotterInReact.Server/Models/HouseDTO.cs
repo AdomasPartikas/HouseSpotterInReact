a93c512481942ca9649fe945da4c10cf72586ac9
Patrikas Adomaitis <ado.patrikas@gmail.com>
2024-02-27 22:20:41 +0200

    New git

---
+++ HouseSpotterInReact.Server/Models/HouseDTO.cs
@@ -0,0 +1,34 @@
+namespace HouseSpotter.Models
+{
+    public class HouseDTO
+    {
+        public string? AnketosKodas { get; set; }
+        public string? Link { get; set; }
+        public string? BustoTipas { get; set; }
+        public string? Title { get; set; }
+        public double? Kaina { get; set; }
+        public double? KainaMen { get; set; }
+        public string? NamoNumeris { get; set; }
+        public string? ButoNumeris { get; set; }
+        public int? KambariuSk { get; set; }
+        public double? Plotas { get; set; }
+        public string? SklypoPlotas { get; set; }
+        public int? Aukstas { get; set; }
+        public int? AukstuSk { get; set; }
+        public int? Metai { get; set; }
+        public string? Irengimas { get; set; }
+        public string? NamoTipas { get; set; }
+        public string? PastatoTipas { get; set; }
+        public string? Sildymas { get; set; }
+        public string? PastatoEnergijosSuvartojimoKlase { get; set; }
+        public string? Ypatybes { get; set; }
+        public string? PapildomosPatalpos { get; set; }
+        public string? PapildomaIranga { get; set; }
+        public string? Apsauga { get; set; }
+        public string? Vanduo { get; set; }
+        public int? IkiTelkinio { get; set; }
+        public string? ArtimiausiasTelkinys { get; set; }
+        public string? RCNumeris { get; set; }
+        public string? Aprasymas { get; set; }
+    }
+}
---
+++ HouseSpotterInReact.Server/Utils/Network/NetworkHttpClient.cs
@@ -0,0 +1,49 @@
+using System.Net;
+
+namespace HouseSpotter.Utils
+{
+    public class NetworkHttpClient
+    {
+        public async Task Destroy()
+        {
+            if (HtmlClient != null)
+            {
+                HtmlClient.Dispose();
+            }
+            HtmlClientInitialized = false;
+        }
+        public async Task Initialize()
+        {
+            // Set up the browser for HtmlClient
+            HtmlClientHandler = new HttpClientHandler
+            {
+                UseProxy = false,
+                UseDefaultCredentials = true,
+                UseCookies = true
+            };
+
+            HtmlClient = new HttpClient(HtmlClientHandler);
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+            HtmlClient.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgents.GetRandomUserAgent());
+            HtmlClient.DefaultRequestHeaders.Accept.Clear();
+            //Client.DefaultRequestHeaders.Add("accept", @"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
+            HtmlClient.DefaultRequestHeaders.Add("upgrade-insecure-requests", "1");
+            HtmlClient.DefaultRequestHeaders.Add("sec-ch-ua-mobile", "?0");
+            HtmlClient.DefaultRequestHeaders.Add("sec-ch-ua-arch", "x86");
+            HtmlClient.DefaultRequestHeaders.Add("sec-ch-ua-wow64", "?0");
+            HtmlClient.DefaultRequestHeaders.Add("sec-ch-ua-bitness", "112.0.5615.165");
+            HtmlClient.DefaultRequestHeaders.Add("sec-ch-ua-full-version", "64");
+            HtmlClient.DefaultRequestHeaders.Add("sec-ch-ua-platform", "Linux");
+            HtmlClient.DefaultRequestHeaders.Add("sec-ch-ua-platform-version", "5.14.0");
+            HtmlClient.DefaultRequestHeaders.Add("sec-fetch-site", "none");
+            HtmlClient.DefaultRequestHeaders.Add("sec-fetch-mode", "navigate");
+            HtmlClient.DefaultRequestHeaders.Add("sec-fetch-user", "?1");
+            HtmlClient.DefaultRequestHeaders.Add("sec-ch-ua", "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \"Not:A-Brand\";v=\"99\"");
+            //Set up the browser for HtmlClient
+            HtmlClientInitialized = true;
+        }
+        public bool HtmlClientInitialized = false;
+        public HttpClientHandler? HtmlClientHandler;
+        public HttpClient? HtmlClient;
+    }
+}
