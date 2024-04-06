1e4028be95108558a212d3506d6dbac877372680
Martynas Zaveckis <martynas@enternet.lt>
2024-04-06 20:12:05 +0300

    Front login functionality added (HOUS-12)

--- housespotterinreact.client/src/App.jsx
+++ housespotterinreact.client/src/App.jsx
@@ -1,20 +1,24 @@
-import { useEffect, useState } from "react";
-import './assets/styles/app.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Product from './pages/Product';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import "./assets/styles/app.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Favorite from "./pages/Favorite";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/prisijungti" element={<Login />} />
-        <Route path="/registruotis" element={<Register />} />
-        <Route path="/skelbimas/:productId" element={<Product />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/prisijungti" element={<Login />} />
+          <Route path="/registruotis" element={<Register />} />
+          <Route path="/megstamiausi" element={<Favorite />} />
+          <Route path="/skelbimas/:productId" element={<Product />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
 }
 
