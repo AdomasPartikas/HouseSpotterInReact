cdd3789d31c40837547e16a52eff0239ba23e13e
Martynas Zaveckis <martynas@enternet.lt>
2024-04-06 20:20:14 +0300

    Front login functionality header component update (HOUS-12)

--- housespotterinreact.client/src/pages/Register.jsx
+++ housespotterinreact.client/src/pages/Register.jsx
@@ -6,41 +6,77 @@ import TextInput from "../components/form/TextInput";
 function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    if (name === "email") setEmail(value);
-    else if (name === "password") setPassword(value);
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      case "name":
+        setUsername(value);
+        break;
+      case "phone":
+        setPhoneNumber(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const payload = {
+      username,
+      password,
+      email,
+      phoneNumber,
+      isAdmin: false
+    };
+
+    try {
+      const response = await fetch('https://127.0.0.1:5016/housespotter/db/user/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json-patch+json',
+        },
+        body: JSON.stringify(payload),
+        mode: 'no-cors',
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        window.location.href = "/prisijungti";
+      } else {
+        const errorData = await response.json();
+        console.error(errorData);
+        alert(errorData.message);
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Failed to register');
+    }
   };
 
   return (
     <div className="login">
-      <Header isAdmin={false} isLoggedIn={false} />
+      <Header />
       <div className="hero">
         <div className="layout">
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <h1>Registruotis</h1>
-            <TextInput
-              label="El. paštas"
-              name="email"
-              placeholder="Įveskite savo el. paštą"
-              inputType="email"
-              onChange={handleInputChange}
-            />
-            <TextInput
-              label="Slaptažodis"
-              name="password"
-              placeholder="Įveskite savo slaptažodį"
-              inputType="password"
-              onChange={handleInputChange}
-            />
-            <button type="submit" className="primary__btn">
-              Registruotis
-            </button>
+            <TextInput label="Vardas" name="name" placeholder="Įveskite savo vardą" inputType="text" onChange={handleInputChange} />
+            <TextInput label="El. paštas" name="email" placeholder="Įveskite savo el. paštą" inputType="email" onChange={handleInputChange} />
+            <TextInput label="Telefonas" name="phone" placeholder="Įveskite savo tel. nr." inputType="text" onChange={handleInputChange} />
+            <TextInput label="Slaptažodis" name="password" placeholder="Įveskite savo slaptažodį" inputType="password" onChange={handleInputChange} />
+            <button type="submit" className="primary__btn">Registruotis</button>
             <p>Jau turite paskyrą?</p>
-            <Link to="/prisijungti" className="secondary__btn">
-              Prisijungti
-            </Link>
+            <Link to="/prisijungti" className="secondary__btn">Prisijungti</Link>
           </form>
         </div>
       </div>
