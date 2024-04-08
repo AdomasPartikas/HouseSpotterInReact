c80bd6c2cf1a06ddb422e311aa2b4d322d17838b
Patrikas Adomaitis <ado.patrikas@gmail.com>
2024-04-09 00:53:48 +0300

    Aruodo testai.

--- Tests/HouseSpotter.Unit/Utils/StaticFixtures.cs
+++ Tests/HouseSpotter.Unit/Utils/StaticFixtures.cs
@@ -6,12 +6,13 @@ namespace HouseSpotter.Unit.Fixtures
     {
         public string SkelbiuPositiveHtml;
         public string DomoPositiveHtml;
+        public string AruodasPositiveHtml;
 
         public StaticFixtures()
         {
             var outputDirectory = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "../../../"));
-            var filePath = Path.Combine(outputDirectory, "Utils", "Htmls", "SkelbiuPositiveHtml.txt");
-            SkelbiuPositiveHtml = File.ReadAllText(filePath);
+            SkelbiuPositiveHtml = File.ReadAllText(Path.Combine(outputDirectory, "Utils", "Htmls", "SkelbiuPositiveHtml.txt"));
+            AruodasPositiveHtml = File.ReadAllText(Path.Combine(outputDirectory, "Utils", "Htmls", "AruodasPositiveHtml.txt"));
             DomoPositiveHtml = File.ReadAllText(Path.Combine(outputDirectory, "Utils", "Htmls", "DomoPositiveHtml.txt"));
         }
     }
