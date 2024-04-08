6f67b59383035f226286a773186642b115071609
Patrikas Adomaitis <ado.patrikas@gmail.com>
2024-04-09 00:53:48 +0300

    Aruodo testai.

---
+++ Tests/HouseSpotter.Unit/AruodoScraperTest.cs
@@ -0,0 +1,40 @@
+namespace HouseSpotter.Unit.Tests
+{
+    public class ScraperForAruodasTests : IClassFixture<ScraperForAruodasFixture>
+    {
+        private readonly ScraperForAruodas _scraperForAruodas;
+        private readonly HousingContext _mockedHousingContext;
+        private readonly ScraperClient _scraperClient;
+
+        public ScraperForAruodasTests(ScraperForAruodasFixture fixture)
+        {
+            _scraperForAruodas = fixture.scraperForAruodas;
+            _mockedHousingContext = fixture.mockedHousingContext;
+            _scraperClient = fixture.scraperClient;
+        }
+
+        [Fact]
+        public async Task FindAllHousingPosts_ShouldPopulateHousingContextWithNewPosts()
+        {
+            // Arrange
+            var mockNetworkPuppeteerClient = new Mock<NetworkPuppeteerClient>();
+            var staticFixtures = new StaticFixtures();
+
+            mockNetworkPuppeteerClient.Setup(client => client.PuppeteerInitialized).Returns(true);
+            mockNetworkPuppeteerClient.Setup(client => client.Initialize()).Returns(Task.CompletedTask);
+            mockNetworkPuppeteerClient.Setup(client => client.PuppeteerPage!.GoToAsync(It.IsAny<string>(), null, null));
+
+            mockNetworkPuppeteerClient.Setup(client => client.PuppeteerPage!.GetContentAsync())
+                                      .ReturnsAsync(staticFixtures.SkelbiuPositiveHtml);
+
+            _scraperClient.NetworkPuppeteerClient = mockNetworkPuppeteerClient.Object;
+
+            // Act
+            var result = await _scraperForAruodas.FindAllHousingPosts();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("Scrape finished successfully", result.Message);
+        }
+    }
+}
--- Tests/HouseSpotter.Unit/Utils/StaticFixtures.cs
+++ Tests/HouseSpotter.Unit/Utils/StaticFixtures.cs
@@ -5,12 +5,13 @@ namespace HouseSpotter.Unit.Fixtures
     public class StaticFixtures
     {
         public string SkelbiuPositiveHtml;
+        public string AruodasPositiveHtml;
 
         public StaticFixtures()
         {
             var outputDirectory = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "../../../"));
-            var filePath = Path.Combine(outputDirectory, "Utils", "Htmls", "SkelbiuPositiveHtml.txt");
-            SkelbiuPositiveHtml = File.ReadAllText(filePath);
+            SkelbiuPositiveHtml = File.ReadAllText(Path.Combine(outputDirectory, "Utils", "Htmls", "SkelbiuPositiveHtml.txt"));
+            AruodasPositiveHtml = File.ReadAllText(Path.Combine(outputDirectory, "Utils", "Htmls", "AruodasPositiveHtml.txt"));
         }
     }
 }
