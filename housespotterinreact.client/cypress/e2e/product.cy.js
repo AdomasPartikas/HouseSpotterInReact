d0dea5bab5b944dd8196a1b17b5c0ac1ee3d831f
Martynas Zaveckis <130957529+martynasenternet@users.noreply.github.com>
2024-04-20 23:41:42 +0300

    Martyno features (#30)
    * feature ci added
    
    * Homepage refactoring + filters added (HOUS-25)
    
    * Front imports fixes
    
    * notification fix for build
    
    * Front scraper page added for admin (HOUS-14)
    
    * Front Martyno E2E
    
    * Case sensitive fixes for git
    
    * Case sensitive fixes for git pt. 2

---
+++ housespotterinreact.client/cypress/e2e/product.cy.js
@@ -0,0 +1,27 @@
+describe('Product page functionality', () => {
+  beforeEach(() => {
+    cy.visit('/skelbimas/3');
+    cy.intercept('GET', '/housespotter/db/getallhousing').as('getProductDetails');
+  });
+
+  it('Produkto detalės atvaizduojamos tinkamai', () => {
+    cy.wait('@getProductDetails');
+
+    // Check for the presence of product attributes
+    cy.get('.product__block').within(() => {
+      cy.get('.product__photo').should('exist');
+      cy.get('h1 a').should('have.attr', 'href');
+      cy.get('.product__content').should('contain', 'Būsto tipas:');
+    });
+  });
+
+  it('Produkto aprašymas atvaizduojamas tinkamai', () => {
+    cy.wait('@getProductDetails');
+
+    // Check for description
+    cy.get('.product__description').within(() => {
+      cy.get('h2').should('contain', 'Aprašymas');
+      cy.get('p').should('exist');
+    });
+  });
+});
---
+++ housespotterinreact.client/cypress/e2e/scraper.cy.js
@@ -0,0 +1,78 @@
+describe('Scraper page functionality', () => {
+  beforeEach(() => {
+    cy.visit('/scraper');
+  });
+
+  it('Atvaizduoja visų scraperių formas', () => {
+    cy.get('.scraper-form').should('have.length', 3); // Assuming there are 3 forms
+  });
+
+  const scraperTests = [
+    { title: 'Domoplius', allUrl: '/housespotter/scrapers/domo/findhousing/all', recentUrl: '/housespotter/scrapers/domo/findhousing/recent', enrichUrl: '/housespotter/scrapers/domo/enrichhousing' },
+    { title: 'Aruodas', allUrl: '/housespotter/scrapers/aruodas/findhousing/all', recentUrl: '/housespotter/scrapers/aruodas/findhousing/recent', enrichUrl: '/housespotter/scrapers/aruodas/enrichhousing' },
+    { title: 'Skelbiu', allUrl: '/housespotter/scrapers/skelbiu/findhousing/all', recentUrl: '/housespotter/scrapers/skelbiu/findhousing/recent', enrichUrl: '/housespotter/scrapers/skelbiu/enrichhousing' },
+  ];
+
+  scraperTests.forEach((scraper) => {
+    it(`Palaiko visų skelbimų scrapinimą - ${scraper.title}`, () => {
+      // Stub the POST request
+      cy.intercept('POST', scraper.allUrl, {
+        statusCode: 200,
+        body: { scrapeStatus: 'Success', message: 'Scrape completed' },
+      }).as('scrapeAllHousing');
+
+      // Click the button to scrape all housing
+      cy.contains('h2', scraper.title)
+        .parent('.scraper-form')
+        .contains('button', 'Find all housing')
+        .click();
+
+      // Wait for the request to complete
+      cy.wait('@scrapeAllHousing');
+
+      // Check for a success notification
+      cy.contains('Operacija pavyko').should('be.visible');
+    });
+
+    it(`Palaiko naujausių skelbimų scrapinimą - ${scraper.title}`, () => {
+      // Stub the POST request
+      cy.intercept('POST', scraper.recentUrl, {
+        statusCode: 200,
+        body: { scrapeStatus: 'Success', message: 'Scrape completed' },
+      }).as('scrapeRecentHousing');
+
+      // Click the button to scrape all housing
+      cy.contains('h2', scraper.title)
+        .parent('.scraper-form')
+        .contains('button', 'Find recent housing')
+        .click();
+
+      // Wait for the request to complete
+      cy.wait('@scrapeRecentHousing');
+
+      // Check for a success notification
+      cy.contains('Operacija pavyko').should('be.visible');
+    });
+
+    it(`Palaiko skelbimų enrichinimą scrapinimą - ${scraper.title}`, () => {
+      // Stub the POST request
+      cy.intercept('POST', scraper.enrichUrl, {
+        statusCode: 200,
+        body: { scrapeStatus: 'Success', message: 'Scrape completed' },
+      }).as('scrapeEnrichHousing');
+
+      // Click the button to scrape all housing
+      cy.contains('h2', scraper.title)
+        .parent('.scraper-form')
+        .contains('button', 'Enrich housing')
+        .click();
+
+      // Wait for the request to complete
+      cy.wait('@scrapeEnrichHousing');
+
+      // Check for a success notification
+      cy.contains('Operacija pavyko').should('be.visible');
+    });
+    
+  });
+});
