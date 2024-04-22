c5cc4fe7bbdc1d7754f5cdbd13d91bfd35057196
Patrikas Adomaitis <monstras789@gmail.com>
2024-04-22 22:08:51 +0300

    Update/Cypress (#34)
    * Cypress now creates reports
    
    * Updated notification.test.jsx since the naming now is changed

--- housespotterinreact.client/cypress.config.js
+++ housespotterinreact.client/cypress.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
+import cypressMochawesomeReporterPlugin from 'cypress-mochawesome-reporter/plugin.js';
 
 export default defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) { },
     baseUrl: 'https://localhost:5173',
     supportFile: 'cypress/support/e2e.js'
   },
-})
+});
